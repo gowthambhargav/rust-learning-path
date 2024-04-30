f82c0f75c34e1abd5dfb3c457202c0c480f43ada
gowthambhargav <gowthambhargav26@gmail.com>
2024-04-30 19:18:32 +0530

    for loops

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,36 +1,20 @@
-struct Car {
-    color: String,
-    transmission:Transmission,
-    convertible:bool,
-    miliage:u32,
-}
-
-
-#[derive(PartialEq,Debug)]
-
-enum Transmission {
-    Manual,
-    SemiAuto,
-    Automatic,
-}
-
-fn car_factory(color:String,transmission:Transmission,convertible:bool)-> Car{
-
-    Car{color,convertible,transmission,miliage:0}
-   
-}
-
-
 
 fn main() {
-    // We have orders for three new cars!
-    // We'll declare a mutable car variable and reuse it for all the cars
-    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
-    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.miliage);
+    //  for i in 0..11{
+    //     print!("{} ",i)
+    //  }
+let sentence = String::from("my first for loop with iteration in string");
+let first_word =get_first_word(sentence);
+print!("{}",first_word)
+}
 
-    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
-    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.miliage);
-
-    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
-    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.miliage);   
+fn get_first_word(sentence:String)->String{
+    let mut ans = String::from("");
+    for char in sentence.chars(){
+        ans.push_str(char.to_string().as_str());
+        if char ==' '{
+            break;
+        }
+    }
+    return  ans;
 }
