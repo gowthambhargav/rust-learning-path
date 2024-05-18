9c6c711dc93f3ae88ae231161854f251bfde3440
gowthambhargav <gowthambhargav26@gmail.com>
2024-05-18 19:45:56 +0530

    Refactor main.rs to use mutable reference in update_str function

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,20 +1,9 @@
-
-fn main() {
-    //  for i in 0..11{
-    //     print!("{} ",i)
-    //  }
-let sentence = String::from("my first for loop with iteration in string");
-let first_word =get_first_word(sentence);
-print!("{}",first_word)
+fn main (){
+    let mut c = String::from("value");
+    update_str(&mut c);
+    println!("{}",c)
 }
 
-fn get_first_word(sentence:String)->String{
-    let mut ans = String::from("");
-    for char in sentence.chars(){
-        ans.push_str(char.to_string().as_str());
-        if char ==' '{
-            break;
-        }
-    }
-    return  ans;
+fn update_str(str:&mut String){
+    str.push_str(" world")
 }
